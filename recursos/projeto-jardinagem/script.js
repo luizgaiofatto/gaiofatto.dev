831e4bce656e9947ae3b3250369746da4e7fcb62
Luiz Felipe Gaiofatto <luizgaiofatto20@gmail.com>
2026-04-02 16:46:59 -0300

    adição de projeto

---
+++ recursos/projeto-jardinagem/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('nav a').forEach(link => { 
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const id = this.getAttribute('href');
+    const section = document.querySelector(id);
+    
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const elementPosition = section.offsetTop;
+    const offsetPosition = elementPosition - headerHeight;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
+    });
+  });
+});
